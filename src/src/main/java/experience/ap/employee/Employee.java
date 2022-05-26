470010e77c8661a21347d8aa9ef574eb4831d778
anoopkrishnadas04 <anoopkrishnadas04@gmail.com>
2022-05-26 07:32:08 -0400

    Employee class commented and added the HashMap attributes and import statements

--- src/src/main/java/experience/ap/employee/Employee.java
+++ src/src/main/java/experience/ap/employee/Employee.java
@@ -1,26 +1,44 @@
 package experience.ap.employee;
+import experience.ap.shift.Shift;
+import java.util.ArrayList;
+import java.util.HashMap;
+@SuppressWarnings("FieldCanBeLocal")
 
 public class Employee {
+    //name  will be the way to refer to the employee
+    //on the Excel file at the end
     private final String name;
+    //phone number is for the convenience of the boss
+    //for the end result of the project
     private final String phoneNumber;
-
-
+    //availSchedule is when the employee is available to work
+    //This takes the key (dayOfWeek - String) and returns an
+    //arrayList of Shift objects according to when they are free
+    private final HashMap<String, ArrayList<Shift>> availSchedule;
+    //assignedSchedule will be what is finally put out for the
+    //employer to see, using the availSchedule as reference
+    private HashMap<String, ArrayList<Shift>> assignedSchedule;
+    //I think your dad said we don't need a no-args
+    //constructor for the Employee class, so I
+    //commented it out. If we need it just
+    //uncomment it by removing the "/**/"
+    //from lines 25 and 30
+    /*
     public Employee(){
         name = "";
         phoneNumber="";
     }
-
+    */
+    //args-constructor so only full people can be created
+    //incomplete Employees will disrupt the future code
     public Employee(String n, String pN){
         name = n;
         phoneNumber = pN;
+        //need to create a way to read in an Excel Sheet and fill this attribute
+        availSchedule = null;
     }
-
-    public String getName(){
-        return name;
-    }
-
-    public String getPhoneNumber(){
-        return phoneNumber;
-    }
-
+    //getName method to access name attribute
+    public String getName(){return name;}
+    //getPhoneNumber method to access phoneNumber attribute
+    public String getPhoneNumber(){return phoneNumber;}
 }
