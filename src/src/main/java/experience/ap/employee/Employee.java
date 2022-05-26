b68be6d08b73b72602390d78d468f45f521eb21e
cpiedrah <100087021+cpiedrah@users.noreply.github.com>
2022-05-25 21:19:49 -0400

    filling in employee object

--- src/src/main/java/experience/ap/employee/Employee.java
+++ src/src/main/java/experience/ap/employee/Employee.java
@@ -1,4 +1,26 @@
 package experience.ap.employee;
 
 public class Employee {
+    private final String name;
+    private final String phoneNumber;
+
+
+    public Employee(){
+        name = "";
+        phoneNumber="";
+    }
+
+    public Employee(String n, String pN){
+        name = n;
+        phoneNumber = pN;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
 }
